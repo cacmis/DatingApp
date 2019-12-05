ad278b813531d3ac0b5afaccb43ccd2ba2ade6c9
Misael Cazarez <cacmis@la-MacBook-Pro-de-Misael.local>
2019-12-04 22:13:52 -0600

    End of Section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -19,6 +19,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
